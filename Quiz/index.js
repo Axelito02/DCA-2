9ad3109bef0435242f2b9c40db3420b8ee6d1a62
Alexander <98567345+Axelito02@users.noreply.github.com>
2023-02-13 21:05:35 -0500

    feat: primer quiz

---
+++ Quiz/index.js
@@ -0,0 +1,14 @@
+function sumarN (n) {
+    switch (n) {
+        case 3:
+            n %3
+            console.log("by 3");
+            break;
+
+        case 5:
+            n % 5
+            console.log("by 5");
+            break;
+            de
+    }
+}
