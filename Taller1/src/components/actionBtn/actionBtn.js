c62689ca0a6597c79be414f515c5b484a7fb198a
Alexander <98567345+Axelito02@users.noreply.github.com>
2023-03-11 15:32:04 -0500

    comp btns

--- Taller1/src/components/actionBtn/actionBtn.js
+++ Taller1/src/components/actionBtn/actionBtn.js
@@ -10,8 +10,27 @@ class Btns extends HTMLElement {
 
     render(){
         this.shadowRoot.innerHTML = `
-        <p>component btns</p>`
+        <link rel="stylesheet" href="./styles/actionBtn.css">
+        <div class= "content">
+            <div class= "switch-btn">
+                <div class="switch">
+                    <input type="checkbox">Music</input>
+                </div>
+                <div class="switch">
+                    <input type="checkbox">Pose Animation</input>
+                </div>
+            </div>
+            <div class= "switch-btn">
+                <div class="switch">
+                    <button class="btn-1">Preview</buttonPreview>
+                </div>
+                <div class="switch">
+                    <button class="btn-2">OBJECTION!</button>
+                </div>
+            </div>
+        </div>    
+        `
     }
 }
 
-customElements.define('comp-btns', Btns)
+customElements.define('comp-btns', Btns);
